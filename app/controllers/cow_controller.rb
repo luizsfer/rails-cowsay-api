d9338c55678e44bcff1c40b1fd7dbd684dff3f81
Luiz Ferreira <luiz.ferreira@outlook.com>
2020-05-24 19:58:32 -0300

    First commit

---
+++ app/controllers/cow_controller.rb
@@ -0,0 +1,13 @@
+# Documentation
+class CowController < ApplicationController
+  def say
+    params.require(:message)
+    params.permit(:cow, :ballon_type, :face_type)
+    message      = params[:message]
+    cow          = params[:cow] || 'cow'
+    balloon_type = params[:ballon_type] || 'say'
+    face_type    = params[:face_type] || 'default'
+
+    @message = Cow.new(cow: cow, face_type: face_type).say(message, balloon_type)
+  end
+end
